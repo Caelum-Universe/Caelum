f524c37f72f5414ddb4b5b3235d5e75fc4680c09
Newt <dragonminer101@gmail.com>
2021-07-27 16:29:22 +0100

    fix: missed conflict and         'version' => '1.0.0',

--- config/lorekeeper/extension_tracker.php
+++ config/lorekeeper/extension_tracker.php
@@ -38,6 +38,7 @@ return [
         'creators' => json_encode([
             'Pure09' => 'https://github.com/deep-ci/',
         ]),
+        'version' => '1.0.0',
     ],
     
     'is_online' => [
